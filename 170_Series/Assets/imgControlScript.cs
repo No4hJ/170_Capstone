c5dbadaedd3de2b6b9bb59773a0de093fc4a4ae0
Yuwei Mao <ymao30@ucsc.edu>
2022-02-21 20:35:56 -0800

    add time to wait of images

--- 170_Series/Assets/imgControlScript.cs
+++ 170_Series/Assets/imgControlScript.cs
@@ -11,6 +11,8 @@ public class imgControlScript : MonoBehaviour
     public GameObject img3;
     public GameObject img4;
     private int imgActive = 0;
+    private static float waitTimeSet = 2.0f; // Set Wait Time Here!
+    private float waitTime = waitTimeSet;
     void Start()
     {
         
@@ -19,6 +21,8 @@ public class imgControlScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      waitTime -= Time.deltaTime;
+      //Debug.Log(Time.deltaTime);
         if (Input.GetKeyDown (KeyCode.W)){
             if (imgActive == 0){
                 imgActive = 1;
@@ -35,17 +39,20 @@ public class imgControlScript : MonoBehaviour
         }
 
         if (imgActive == 1){
-            if (Input.GetMouseButtonDown(0)){
+            if (Input.GetMouseButtonDown(0) && waitTime <=0){
 			       //Debug.Log("Pressed primary button.");
              if (img1.activeSelf){
                img1.SetActive(false);
                img2.SetActive(true);
+               waitTime = waitTimeSet;
              }else if (img2.activeSelf){
                img2.SetActive(false);
                img3.SetActive(true);
+               waitTime = waitTimeSet;
              }else if (img3.activeSelf){
                img3.SetActive(false);
                img4.SetActive(true);
+               waitTime = waitTimeSet;
              }else if (img4.activeSelf){
                img4.SetActive(false);
                room.SetActive(true);
