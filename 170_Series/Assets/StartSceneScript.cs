a5c64753542652003937f1278583672ad7083ccf
Yuwei Mao <ymao30@ucsc.edu>
2022-03-01 21:37:16 -0800

    localization and basic initialization of the start scene

---
+++ 170_Series/Assets/StartSceneScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Begin(){
+        Debug.Log("Begin");
+    }
+
+    public void Option(){
+        Debug.Log("Option");
+    }
+
+    public void Credit(){
+        Debug.Log("Credit");
+    }
+
+    public void Exit(){
+        Debug.Log("Exit");
+    }
+}
