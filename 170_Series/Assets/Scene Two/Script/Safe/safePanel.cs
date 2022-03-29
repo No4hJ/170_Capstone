7526e79dff6c526d2cb1fca2b62c62161465ed90
Yuwei Mao <ymao30@ucsc.edu>
2022-03-29 01:15:05 -0700

    add puzzle &  password time delay

--- 170_Series/Assets/Scene Two/Script/Safe/safePanel.cs
+++ 170_Series/Assets/Scene Two/Script/Safe/safePanel.cs
@@ -10,6 +10,10 @@ public class safePanel : MonoBehaviour
     public GameObject safe;
     public GameObject inside;
     string currentValue = "";
+    private static float waitTimeSet = 1.0f; // Set Wait Time Here!
+    private float waitTime = waitTimeSet;
+    private bool editable = true;
+    private string correctPassword = "1234"; // Set Password Here!
     void Start()
     {
         
@@ -21,43 +25,65 @@ public class safePanel : MonoBehaviour
         passwordText.text = currentValue;
 
         // Change Password Here!
-        if (currentValue == "1234"){
-            safe.SetActive(false);
-            inside.SetActive(true);
-            gameObject.SetActive(false);
+        // if (currentValue == correctPassword){
+        //     //currentValue = "CORRECT";
+        //     editable = false;
+        // }
+
+        if (editable){
+            if (currentValue.Length >=4){
+                //currentValue = "WRONG";
+                editable = false;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
+            {
+                currentValue += "0";
+            }else if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1)){
+                currentValue += "1";
+            }else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2)){
+                currentValue += "2";
+            }else if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3)){
+                currentValue += "3";
+            }else if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4)){
+                currentValue += "4";
+            }else if (Input.GetKeyDown(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.Keypad5)){
+                currentValue += "5";
+            }else if (Input.GetKeyDown(KeyCode.Alpha6) || Input.GetKeyDown(KeyCode.Keypad6)){
+                currentValue += "6";
+            }else if (Input.GetKeyDown(KeyCode.Alpha7) || Input.GetKeyDown(KeyCode.Keypad7)){
+                currentValue += "7";
+            }else if (Input.GetKeyDown(KeyCode.Alpha8) || Input.GetKeyDown(KeyCode.Keypad8)){
+                currentValue += "8";
+            }else if (Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Keypad9)){
+                currentValue += "9";
+            }
         }
 
-        if (currentValue.Length >=4){
-            currentValue = "";
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
-        {
-            currentValue += "0";
-        }else if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1)){
-            currentValue += "1";
-        }else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2)){
-            currentValue += "2";
-        }else if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3)){
-            currentValue += "3";
-        }else if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4)){
-            currentValue += "4";
-        }else if (Input.GetKeyDown(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.Keypad5)){
-            currentValue += "5";
-        }else if (Input.GetKeyDown(KeyCode.Alpha6) || Input.GetKeyDown(KeyCode.Keypad6)){
-            currentValue += "6";
-        }else if (Input.GetKeyDown(KeyCode.Alpha7) || Input.GetKeyDown(KeyCode.Keypad7)){
-            currentValue += "7";
-        }else if (Input.GetKeyDown(KeyCode.Alpha8) || Input.GetKeyDown(KeyCode.Keypad8)){
-            currentValue += "8";
-        }else if (Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Keypad9)){
-            currentValue += "9";
+        if (!editable){
+            waitTime -= Time.deltaTime;
+            if (waitTime <=0){
+                editable = true;
+                waitTime = waitTimeSet;
+                if(currentValue == correctPassword){
+                    unlock();
+                }
+                currentValue = "";
+            }
         }
         
     }
 
     public void AddWord(string digit){
-        currentValue += digit;
+        if (editable){
+            currentValue += digit;
+        }
         //Debug.Log(digit);
+    }
+
+    private void unlock(){
+        safe.SetActive(false);
+        inside.SetActive(true);
+        gameObject.SetActive(false);
     }
 }
---
+++ 170_Series/Assets/touchJudgment.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchJudgment : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Color originalColor;
+    void Start()
+    {
+        originalColor = GetComponent<SpriteRenderer>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (GetComponent<Collider2D>().IsTouching(img1.GetComponent<Collider2D>())){
+        //     GetComponent<SpriteRenderer>().color = new Color(1,0,0.3f,1);
+        //     Debug.Log("233");
+        // }else{
+        //     GetComponent<SpriteRenderer>().color = originalColor;
+        // }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (gameObject.name == "ImgBase1" && collision.gameObject.name == "puzzleImg1")
+        {
+            GetComponent<SpriteRenderer>().color = Color.green;
+        }else if (gameObject.name == "ImgBase2" && collision.gameObject.name == "puzzleImg2")
+        {
+            GetComponent<SpriteRenderer>().color = Color.green;
+        }else if (gameObject.name == "ImgBase3" && collision.gameObject.name == "puzzleImg3")
+        {
+            GetComponent<SpriteRenderer>().color = Color.green;
+        }else if (gameObject.name == "ImgBase4" && collision.gameObject.name == "puzzleImg4")
+        {
+            GetComponent<SpriteRenderer>().color = Color.green;
+        }else if (gameObject.name == "ImgBase5" && collision.gameObject.name == "puzzleImg5")
+        {
+            GetComponent<SpriteRenderer>().color = Color.green;
+        }else if (gameObject.name == "ImgBase6" && collision.gameObject.name == "puzzleImg6")
+        {
+            GetComponent<SpriteRenderer>().color = Color.green;
+        }
+    }
+
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        GetComponent<SpriteRenderer>().color = originalColor;
+    }
+}
