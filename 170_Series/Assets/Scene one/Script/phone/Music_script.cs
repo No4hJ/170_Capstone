10aaa8f3d9efa4a6879cbc39c4464ec4215d8438
Unlimited-Zone <lli142@ucsc.edu>
2022-05-14 00:21:35 -0700

    Update

--- 170_Series/Assets/Scene one/Script/phone/Music_script.cs
+++ 170_Series/Assets/Scene one/Script/phone/Music_script.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Music_script : MonoBehaviour
 {
+
+    public GameObject Music_list;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Music_list = this.gameObject.transform.GetChild(1).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Music1(){
+        
+        Debug.Log("music1 on");
+    }
+
+    public void Music2(){
+        Debug.Log("music2 on");
     }
 }
